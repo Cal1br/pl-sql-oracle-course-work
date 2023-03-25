99cd43bd215184b89652f9b4b0afc7da78e3b5fa
Cal1br <atillamexmed3@gmail.com>
2023-03-25 20:52:10 +0200

    initial

---
+++ insert-data.sql
@@ -0,0 +1,26 @@
+nsert into department(dep_name, dep_desc)
+values ('Операции','Операции');
+insert into department(dep_name, dep_desc)
+values ('Кредитен анализ','Кредитен анализ');
+insert into department(dep_name, dep_desc)
+values ('Картови разплащания','Картови разплащания');
+
+insert into employee(egn, first_name, last_name, addl_name, address, phone, email, job_desc, dep_id)
+values ('9515156545','Angel', 'Georgiev', '', 'Bulgaria', '0895465456', 'angelbgpl@gmail.com', 'Operator', 1);
+
+insert into employee (egn, first_name, last_name, addl_name, address, phone, email, job_desc, dep_id)
+values ('9515356545','Vasil', 'Grigorov', '', 'Italy', '0895546456', 'vas@gmail.com', 'Cards', 3);
+
+insert into employee (egn, first_name, last_name, addl_name, address, phone, email, job_desc, dep_id)
+values ('9713156145','Petar', 'Angelov', '', 'Bulgaria', '0895454556', 'Pet@gmail.com', 'Credit operator', 2);
+
+
+insert into customer (egn, first_name, last_name, addl_name, address, phone, email)
+values ('9213156145', 'Ivan', 'Ivanov', '', 'France', '0892165', 'iv@gmail.com');
+
+insert into customer (egn, first_name, last_name, addl_name, address, phone, email)
+values ('9313156145', 'Georgi', 'Petrov', '', 'Bulgaria', '089216655', 'gi@gmail.com');
+
+insert into customer (egn, first_name, last_name, addl_name, address, phone, email)
+values ('9613156145', 'Silvia', 'Ivanova', '', 'Germany', '089215465', 'siv@gmail.com');
+
